2cb6568175e19c2be69297362aab711e754b9b1a
Dmitriy Shilovskiy <dimareectsk@gmail.com>
2023-01-27 20:56:11 +0700

    add routes

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,7 +1,6 @@
 const { Router } = require('express');
 const router = new Router();
-const auth = require('./auth');
 
-router.use('/auth', auth);
+router.post('/regiser');
 
 module.exports = router;
--- server/routes/cartRouter.js
+++ server/routes/cartRouter.js
@@ -1,7 +1,4 @@
 const { Router } = require('express');
 const router = new Router();
-const auth = require('./auth');
-
-router.use('/auth', auth);
 
 module.exports = router;
