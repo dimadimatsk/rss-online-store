0a84b836c73ace7b9d7b6044cb2a2dd86524b791
Dmitriy Shilovskiy <dimareectsk@gmail.com>
2023-01-29 11:55:29 +0700

    add auth controller

---
+++ server/controllers/authController.js
@@ -0,0 +1,34 @@
+const AuthService = require('../services/authService');
+
+class AuthController {
+  async register(req, res, next) {
+    try {
+      const { username, email, password } = req.body;
+      const userData = await AuthService.register(username, email, password);
+      res.cookie('refreshToken', userData.refreshToken, {
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        httpOnly: true,
+      });
+      return res.json(userData);
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
+
+  async login(req, res, next) {
+    try {
+    } catch (error) {}
+  }
+
+  async logout(req, res, next) {
+    try {
+    } catch (error) {}
+  }
+
+  async refresh(req, res, next) {
+    try {
+    } catch (error) {}
+  }
+}
+
+module.exports = new AuthController();
